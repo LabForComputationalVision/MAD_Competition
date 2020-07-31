1812b5afa43894064a8c88b04c323be3b2d5f548
William F. Broderick <billbrod@gmail.com>
2020-07-31 14:55:26 -0500

    replaces ssim weighted with version from paper
    replaces both the function and the derivative with the version from the
    paper: removes the square and changes the weight

--- derivative_ssim_weighted.m
+++ derivative_ssim_weighted.m
@@ -42,15 +42,16 @@ for i=1:M1-W1+1
       MUx = mu1(i,j);
       MUy = mu2(i,j);
 
+      % compared to the notation in the paper (Appendix B), x and y are flipped here
       local_ssim = A1*A2/(B1*B2);
       local_dssim = (2/(W1*W2*B1^2*B2^2)) * (A1*A2*MUx*(B1-B2) + B1*B2*MUy*(A2-A1) + A1*B1*(B2*y - A2*x));
-      local_weight = B2;
-      local_dweight = (2/(W1*W2)) * (x - MUx);
+      local_weight = log((1+(sigma1_sq/C2))*(1+(sigma2_sq/C2)));
+      local_dweight = (2/(W1*W2)) * ((x - MUx) / (sigma1_sq + C2));
  
-		w(i:i+W1-1, j:j+W2-1) = w(i:i+W1-1, j:j+W2-1) + local_weight;
-		w_ssim(i:i+W1-1, j:j+W2-1) = w_ssim(i:i+W1-1, j:j+W2-1) + local_weight*local_ssim;
-		d_w(i:i+W1-1, j:j+W2-1) = d_w(i:i+W1-1, j:j+W2-1) + local_dweight;
- 		d_w_ssim(i:i+W1-1, j:j+W2-1) = d_w_ssim(i:i+W1-1, j:j+W2-1) + local_weight*local_dssim+local_ssim*local_dweight;
+      w(i:i+W1-1, j:j+W2-1) = w(i:i+W1-1, j:j+W2-1) + local_weight;
+      w_ssim(i:i+W1-1, j:j+W2-1) = w_ssim(i:i+W1-1, j:j+W2-1) + local_weight*local_ssim;
+      d_w(i:i+W1-1, j:j+W2-1) = d_w(i:i+W1-1, j:j+W2-1) + local_dweight;
+      d_w_ssim(i:i+W1-1, j:j+W2-1) = d_w_ssim(i:i+W1-1, j:j+W2-1) + local_weight*local_dssim+local_ssim*local_dweight;
       
       swin(i:i+W1-1, j:j+W2-1) = swin(i:i+W1-1, j:j+W2-1) + window;
    end
--- ssim_weighted.m
+++ ssim_weighted.m
@@ -103,9 +103,9 @@ sigma1_sq = filter2(window, img1.*img1, 'valid') - mu1_sq;
 sigma2_sq = filter2(window, img2.*img2, 'valid') - mu2_sq;
 sigma12 = filter2(window, img1.*img2, 'valid') - mu1_mu2;
 
-ssim_map = (((2*mu1_mu2 + C1).*(2*sigma12 + C2))./((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2))).^2;
+ssim_map = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))./((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2));
 
-weight = sigma1_sq + sigma2_sq + C2;
+weight = log((1+(sigma1_sq/C2))*(1+(sigma2_sq/C2)));
 
 mssim = sum(sum(ssim_map.*weight))/sum(sum(weight));
 
