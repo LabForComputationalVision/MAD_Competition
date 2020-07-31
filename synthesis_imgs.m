b41e5c2807c6edd296ad902c9546f3e2e77ff099
William F. Broderick <billbrod@gmail.com>
2020-07-31 18:42:40 -0500

    updates paths for new structure of files

--- synthesis_imgs.m
+++ synthesis_imgs.m
@@ -5,8 +5,8 @@ for i=1:10
    for l=3:10
       sigma2 = 2^l;
       iter = [2 1 1 5];
-      imname = ['results/samp' num2str(i) '.tif'];
-      resname = ['results/samp' num2str(i) '_L' num2str(sigma2) '_results.mat'];
+      imname = ['data/images/samp' num2str(i) '.tif'];
+      resname = ['data/results/samp' num2str(i) '_L' num2str(sigma2) '_results.mat'];
       fungds_mse_ssimweighted(sigma2, iter, imname, resname);
    end
 end
