3bfad121b09c97804885d3077d8f0c54b022b5ce
William F. Broderick <billbrod@gmail.com>
2021-04-22 13:10:22 -0400

    corrects image path in plenoptic_analysis

--- plenoptic_analysis.m
+++ plenoptic_analysis.m
@@ -1,9 +1,9 @@
 % generate SSIM output for comparing to plenoptic
 
 ssim = struct('standard', struct(), 'weighted', struct(), 'base_img', 'samp1.tif');
-base_img = imread(['data/images/' ssim.base_img]);
+base_img = imread(['data/ssim_images/' ssim.base_img]);
 for i=1:10
-    imname = ['data/images/samp' num2str(i) '.tif'];
+    imname = ['data/ssim_images/samp' num2str(i) '.tif'];
     val = ssim_index(base_img, imread(imname));
     eval(['ssim.standard.samp' num2str(i) ' = ' num2str(val) ';']);
     val = ssim_weighted(base_img, imread(imname));
